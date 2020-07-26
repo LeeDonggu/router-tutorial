05978f95ff61c37b1c6f0c4fa4858fb95fe85430
ldg <donggu19940717@gmail.com>
2020-07-26 18:42:11 +0900

    react-router-dom tutorial Success

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+// import Profile from "./Profile";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 const App = () => {
   return (
@@ -14,21 +16,42 @@ const App = () => {
         <li>
           <Link to="/about?detail=true">소개</Link>
         </li>
-        <li>
+        {/* <li>
           <Link to="/profile/leedonggu">leedonggu 프로필</Link>
         </li>
         <li>
           <Link to="/profile/gildong">gildong 프로필</Link>
+        </li> */}
+        {/* 서브 라우트 적용된 컴포넌트 */}
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true} />
-      {/* v5 버전 이전에서 사용되던 하나의 컴포넌트를 여러 경로에서 보여주는 방법 */}
-      {/* <Route path="/about" component={About} /> */}
-      {/* <Route path="/info" component={About} /> */}
-      {/* v5 버전 이후에서 사용되는 하나의 컴포넌트를 여러 경로에서 보여주는 방법 */}
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        {/* v5 버전 이전에서 사용되던 하나의 컴포넌트를 여러 경로에서 보여주는 방법 */}
+        {/* <Route path="/about" component={About} /> */}
+        {/* <Route path="/info" component={About} /> */}
+        {/* v5 버전 이후에서 사용되는 하나의 컴포넌트를 여러 경로에서 보여주는 방법 */}
+        <Route path={["/about", "/info"]} component={About} />
+        {/* <Route path="/profile/:username" component={Profile} /> */}
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+
+        {/* path를 따로 정의하지 않으면 모든 상황에 렌더링됨 */}
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
---
+++ src/Profiles.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavLink, Link, Route } from "react-router-dom";
+import Profile from "./Profile";
+import WithRouterSample from "./WithRouterSample";
+
+const Profiles = () => {
+  const activeStyle = {
+    background: "black",
+    color: "white",
+  };
+  return (
+    <div>
+      <h3>사용자 목록:</h3>
+      <ul>
+        {/* <li>
+          <Link to="/profiles/leedonggu">leedonggu</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong</Link>
+        </li> */}
+        {/* NavLink 적용 */}
+        <li>
+          <NavLink activeStyle={activeStyle} to="/profiles/leedonggu" active>
+            leedonggu
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+            gildong
+          </NavLink>
+        </li>
+      </ul>
+
+      <Route
+        path="/profiles"
+        exact
+        render={() => <div>사용자를 선택해 주세요.</div>}
+      />
+      <Route path="/profiles/:username" component={Profile} />
+
+      <WithRouterSample />
+    </div>
+  );
+};
+
+export default Profiles;
